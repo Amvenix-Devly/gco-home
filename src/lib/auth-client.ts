c00072e60f442179f36481ad718d9db18120d65e
devshahorair <shuvo1556@gmail.com>
2024-12-13 23:28:25 +0600

    s

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,11 @@
 import { createAuthClient } from 'better-auth/react'
 import { APP_URL } from './utils'
 
-import { inferAdditionalFields } from "better-auth/client/plugins";
-import type { auth } from './auth';
+import { inferAdditionalFields } from 'better-auth/client/plugins'
+import type { auth } from './auth'
 
-export const { signIn, signUp, signOut,useSession,getSession } = createAuthClient({
-  baseURL: APP_URL,
-  plugins: [inferAdditionalFields<typeof auth>()],
-})
+export const { signIn, signUp, signOut, useSession, getSession } =
+  createAuthClient({
+    baseURL: APP_URL,
+    plugins: [inferAdditionalFields<typeof auth>()],
+  }) as any
